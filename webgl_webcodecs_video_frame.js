242972eab6c727ff706c3123482a9c57c7653e19
Yan, Shaobo <shaobo.yan@intel.com>
2020-12-16 14:43:11 +0800

    Fix the texImage2D + ImageBitmap path

--- webgl_webcodecs_video_frame.js
+++ webgl_webcodecs_video_frame.js
@@ -341,12 +341,20 @@ function requestWebGLVideoFrameHandler(canvas, stat, renderingMethod) {
         gl.useProgram(program);
         initVertexBuffers(gl, program, frame, "aVertexPosition", "aTexCoord");
 
+        var texture = gl.createTexture();
         gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, tex2DTexture);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
 
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageBitmap);
 
-        //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+       const locTexTransform = gl.getUniformLocation(program, "uTexTransform");
+        let transform = new Float32Array([0, 0, 1, 1]);
+        gl.uniform4fv(locTexTransform, transform);
+
         gl.uniform1i(gl.getUniformLocation(program, "diffuse"), 0);
 
         // Based on the frame's timestamp calculate how much of real time waiting
@@ -354,6 +362,7 @@ function requestWebGLVideoFrameHandler(canvas, stat, renderingMethod) {
         const time_till_next_frame = calculateTimeTillNextFrame(frame.timestamp);
         await delay(time_till_next_frame);
 
+        gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.clearColor(0.0, 0.0, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.TRIANGLES, 0, 6);
